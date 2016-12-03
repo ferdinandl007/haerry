0ca7cc6682acb0c8019a923c8acbabf5027eff65
ferdinandl007 <ferdinand@ferdinands-macbook-pro.local>
2016-12-03 15:00:04 +0000

    Initial Commit

---
+++ haerry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  haerry
+//
+//  Created by Ferdinand Lösch on 03/12/2016.
+//  Copyright © 2016 Ferdinand Lösch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
